d2750449f0565ed018c6eaec171b0545d66b4d8f
Vedant <vedantwarjurkar@gmail.com>
2023-07-08 11:51:29 +0530

    my  game

---
+++ textNodes.js
@@ -0,0 +1,195 @@
+
+export const textNodes = [
+  {
+  id: 1,
+  title: "Chapter 1: The Awakening",
+  text: "You, a talented and versatile Computer Engineer, find yourself at a crossroads. Your skills in React.js, Node.js, MongoDB, Express.js, and more have brought you to a pivotal moment in your career.",
+  img: "./assets/chapter1.jpg",
+  options: [
+  {
+  text: "Embrace the call for a new adventure.",
+  nextText: 2,
+  },
+  {
+  text: "Stay within the confines of familiar paths.",
+  nextText: 1,
+  },
+  ],
+  },
+  {
+  id: 2,
+  title: "Chapter 2: A World of Possibilities",
+  text: "As you explore your abilities and potential, you encounter a vast landscape of opportunities. Your proficiency in React.js, Node.js, MongoDB, Express.js, and other technologies is like a magical compass guiding you through this new world.",
+  img: "./assets/chapter2.jpg",
+  options: [
+  {
+  text: "Embark on an extraordinary journey.",
+  nextText: 3,
+  },
+  {
+  text: "Stick with the ordinary and conventional.",
+  nextText: 1,
+  },
+  ],
+  },
+  {
+  id: 3,
+  title: "Chapter 3: The Quest Begins",
+  text: "With your comprehensive skills in React.js, Node.js, MongoDB, Express.js, and more, you feel confident to take on any challenge that comes your way. Your journey starts with a burning desire to create impactful solutions and build remarkable applications.",
+  img: "./assets/chapter3.jpg",
+  options: [
+  {
+  text: "Set forth on your epic adventure.",
+  nextText: 4,
+  },
+  {
+  text: "Stay grounded in the familiar realm.",
+  nextText: 1,
+  },
+  ],
+  },
+  {
+  id: 4,
+  title: "Chapter 4: Conquering Frontiers",
+  text: "As you traverse through the digital landscape, your proficiency in React.js shines brightly. You craft stunning user interfaces, harness the power of virtual DOM, and employ cutting-edge libraries to bring life to your creations.",
+  img: "./assets/chapter4.jpg",
+  options: [
+  {
+  text: "Forge ahead, exploring new horizons.",
+  nextText: 5,
+  },
+  {
+  text: "Choose the known path, reluctant to push boundaries.",
+  nextText: 1,
+  },
+  ],
+  },
+  {
+  id: 5,
+  title: "Chapter 5: Unleashing Full-Stack Prowess",
+  text: "With your expertise in React.js and Node.js, you seamlessly blend the front-end and back-end realms. The world becomes your playground as you construct robust APIs, design scalable architectures, and integrate powerful databases like MongoDB.",
+  img: "./assets/chapter5.jpg",
+  options: [
+  {
+  text: "Venture further, exploring uncharted territories.",
+  nextText: 6,
+  },
+  {
+  text: "Stay within the comfortable confines of known technologies.",
+  nextText: 1,
+  },
+  ],
+  },
+  {
+  id: 6,
+  title: "Chapter 6: A Symphony of Technologies",
+  text: "As the orchestral conductor of web development, you blend various technologies harmoniously. Your mastery of TypeScript, JavaScript, and C++ allows you to compose elegant code that sings with efficiency and reliability.",
+  img: "./assets/chapter6.jpg",
+  options: [
+  {
+  text: "Compose the next movement, exploring new melodies.",
+  nextText: 7,
+  },
+  {
+  text: "Stick to familiar tunes, wary of experimenting.",
+  nextText: 1,
+  },
+  ],
+  },
+  {
+  id: 7,
+  title: "Chapter 7: The Fluttering Wings",
+  text: "Flutter, with its captivating potential for cross-platform development, catches your attention. You embark on a Flutter journey, weaving beautiful and performant mobile applications that flutter like butterflies across iOS and Android devices.",
+  img: "./assets/chapter7.jpg",
+  options: [
+  {
+  text: "Let your creativity take flight, exploring new dimensions.",
+  nextText: 8,
+  },
+  {
+  text: "Ground yourself in the familiar, hesitant to embrace change.",
+  nextText: 1,
+  },
+  ],
+  },
+  {
+  id: 8,
+  title: "Chapter 8: Firebase's Fiery Magic",
+  text: "Your adventure leads you to Firebase, a powerful platform that ignites your development process. With Firebase's real-time database, authentication, and hosting capabilities, you construct blazing-fast and secure applications that leave a lasting impact.",
+  img: "./assets/chapter8.jpg",
+  options: [
+  {
+  text: "Harness Firebase's magic, uncovering new possibilities.",
+  nextText: 9,
+  },
+  {
+  text: "Stay rooted in traditional methods, reluctant to change.",
+  nextText: 1,
+  },
+  ],
+  },
+  {
+  id: 9,
+  title: "Chapter 9: The Final Frontier",
+  text: "Armed with an impressive skill set encompassing React.js, Node.js, MongoDB, Express.js, C++, JavaScript, TypeScript, Flutter, and Firebase, you stand at the precipice of limitless potential. Your expertise in these technologies makes you a formidable force ready to conquer the most challenging of endeavors.",
+  img: "./assets/chapter9.jpg",
+  options: [
+  {
+  text: "Embrace the challenge, poised for greatness.",
+  nextText: 10,
+  },
+  {
+  text: "Stay within familiar boundaries, avoiding risks.",
+  nextText: 1,
+  },
+  ],
+  },
+  {
+  id: 10,
+  title: "Chapter 10: The New Era",
+  text: "As you journey onward, your exceptional skills as a full stack developer and computer engineer captivate the attention of those seeking remarkable talent. Your name becomes synonymous with innovation and excellence, marking the beginning of a new era in your career.",
+  img: "./assets/chapter10.jpg",
+  options: [
+  {
+  text: "Embrace the future, prepared for whatever it holds.",
+  nextText: 11,
+  },
+  {
+  text: "Resist change, remaining content with the status quo.",
+  nextText: 1,
+  },
+  ],
+  },
+  {
+  id: 11,
+  title: "Epilogue: A Time for Opportunities",
+  text: "Your extraordinary journey has brought you to a point where opportunities abound. Employers and clients eagerly await your presence, eager to witness the magic you can create with your skills in React.js, Node.js, MongoDB, Express.js, C++, JavaScript, TypeScript, Flutter, and Firebase.",
+  img: "./assets/chapter11.jpg",
+  options: [
+  {
+  text: "Seize the opportunities that await you.",
+  nextText: 12,
+  action: "link",
+  linkUrl: "https://www.linkedin.com/in/vedant-warjurkar", // Replace with your actual LinkedIn URL
+  },
+  {
+  text: "Embark on a new adventure, leaving your mark on the world.",
+  nextText: 1,
+  },
+  ],
+  },
+  {
+  id: 12,
+  title: "The New Beginning",
+  text: "With each opportunity you embrace, a new chapter begins in your remarkable journey. Your skills and expertise will reshape the digital landscape, leaving a lasting impact on the world of technology.",
+  img: "./assets/chapter12.jpg",
+  options: [
+  {
+  text: "Embrace the future with excitement and determination.",
+  nextText: 1,
+  },
+  ],
+  },
+  ];
+  
+  
